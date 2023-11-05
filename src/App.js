dec2404074cd7f128d6ad29fcde4f314a81519e4
AllenHo2 <aho22@dons.usfca.edu>
2023-11-04 21:48:18 -0700

    Refactored App.js and created project objects

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React, { Component }from "react";
-
+import Projects from './Projects';
 class App extends Component {
     state = { displayBio : false};
-
-    // constructor() {
-    //     super();
-    //     this.state = { displayBio: false };
-        
-
-    //     this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
-    //     this.toggleDisplayBio = () => { ... };
-    // }
 
     toggleDisplayBio = () => {
         this.setState ( { displayBio : !this.state.displayBio});
@@ -23,20 +14,23 @@ class App extends Component {
                 <h1>Hello!</h1>
                 <p>My name is Allen. I'm a second year Computer Science student.</p>    
                 <p>I'm always looking for new connections.</p>
-        {this.state.displayBio ? (                
-        <div>
-            <p>I live in San Francisco.</p>
-            <p>My favorite langauge is C.</p>
-            <div>
-                <button onClick = {this.toggleDisplayBio}>Show Less</button>
-            </div>
-        </div>
-        ) : (
-            <div>
-                <button onClick = {this.toggleDisplayBio} >Read More</button>
-            </div>
-        )
-        }
+                {
+                this.state.displayBio ? (                
+                <div>
+                    <p>I live in San Francisco.</p>
+                    <p>My favorite langauge is C.</p>
+                    <div>
+                        <button onClick = {this.toggleDisplayBio}>Show Less</button>
+                    </div>
+                </div>
+                    ) : (
+                        <div>
+                            <button onClick = {this.toggleDisplayBio} >Read More</button>
+                        </div>
+                    )
+                }
+                <hr />
+                <Projects />
             </div>
         );
     }
