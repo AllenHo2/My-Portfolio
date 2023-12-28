4fdb8186cf7531794920c66f08d4aacc279824d1
AllenHo2 <aho22@dons.usfca.edu>
2023-12-27 18:16:02 -0800

    removed excess projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,15 +1,15 @@
-import project1 from '../assets/project1.png'
+//import project1 from '../assets/project1.png'
 import Elevator from '../assets/Elevator.png'   
 //import project3 from '../assets/project3.png'
 
 const PROJECTS = [
-    {
-        id: 1,
-        title: 'Example React Application',
-        description: 'A React App that I built, involving JS and core web dev concepts!',
-       // link: 'https://github.com/cs221-0203-f22/project03-AllenHo2',
-        image: project1
-    },
+    // {
+    //     id: 1,
+    //     title: 'Example React Application',
+    //     description: 'A React App that I built, involving JS and core web dev concepts!',
+    //    // link: 'https://github.com/cs221-0203-f22/project03-AllenHo2',
+    //     image: project1
+    // },
     {
         id: 2,
         title: 'Elevator Simulation',
