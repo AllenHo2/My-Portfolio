cf518ca8c63add1a959088107bc51df9c7583615
AllenHo2 <aho22@dons.usfca.edu>
2024-01-16 18:52:01 -0800

    Added 10 More Jokes Array Map while also refactoring the setup into a Joke Component that can be reused between each methods

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
 
+const Joke = ({ joke : {setup, punchline }}) => (
+    <p style={{ margin: 20 }}>{setup} <em>{punchline}</em></p>
+)
+
 class Jokes extends Component {
-    state = { joke: {} };
+    state = { joke: {}, jokes:[] };
 
     componentDidMount() {
         fetch('https://official-joke-api.appspot.com/random_joke')
-            .then(response => response.json() )
+            .then(response => response.json())
             .then(json => this.setState({ joke: json}));
     }
 
-    render() {
-        const { setup, punchline } = this.state.joke; 
+    fetchJokes = () => {
+        fetch('https://official-joke-api.appspot.com/random_ten')
+            .then(response => response.json())
+            .then(json => this.setState({ jokes : json}));
+    }
 
+    render() {
         return(
             <div>
                 <h2>Highlighted Jokes</h2>
-                <div>{setup} <em>{punchline}</em> </div>
+                <Joke joke={this.state.joke}/>
+                <h3>Want More Jokes?</h3>
+                <br />
+                <button onClick={this.fetchJokes}>Click Me</button>
+                {this.state.jokes.map(joke => (<Joke key={joke.id} joke={joke} />))}
             </div>
         )
     }
